861e0d8525132571102f414d4d3d7ec870ab5613
Tenchiroxx <emmanuelgardin@hotmail.fr>
2021-04-12 03:33:05 +0200

    Commenting load_data.py for salinas

--- salinas_utils/load_data.py
+++ salinas_utils/load_data.py
@@ -1,3 +1,7 @@
+"""
+This code is specifically designed to open Salinas data, preprocess it, and separate it into training, validation, and test sets.
+"""
+
 from sklearn.decomposition import PCA 
 from scipy.io import loadmat
 import tensorflow as tf
@@ -8,12 +12,18 @@ import numpy as np
 
 def load(activate_pca = True, pca_explained_variance = 0.9999):
 
+    """
+    This function loads the data, preprocesses it, and creates training, validation and test sets. 
+    """
+
     ## -- Loading data and labels --
 
     data = loadmat('salinas/Salinas_corrected.mat')['salinas_corrected']
     labels = loadmat('salinas/Salinas_gt.mat')['salinas_gt']
 
 
+    # If PCA is activated, the dimension of each pixel will be reduced
+    # For example, after a PCA with 99.99% of explained variance, the number of bands for each pixel will be 29.
     if activate_pca :
         data_shape = data.shape
         data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
@@ -21,11 +31,7 @@ def load(activate_pca = True, pca_explained_variance = 0.9999):
         data = pca.fit_transform(data)
         data = data.reshape((data_shape[0], data_shape[1], data.shape[1]))
         del pca
-    # Merging redundant classes
 
-    # simplifier = {0:0, 1:1, 2:1, 3:2, 4:2, 5:2, 6:3, 7:4, 8:5, 9:5, 10:6, 11:7, 12:7, 13:7, 14:7, 15:5, 16:5} 
-    # for key in simplifier.keys():
-    #     labels[labels == key] = simplifier[key]
 
     # Extracting patches from the image
     ksize_row, ksize_col = 32, 32
@@ -45,13 +51,8 @@ def load(activate_pca = True, pca_explained_variance = 0.9999):
     coords = [(i, j) for i in range (0,16) for j in range(0,6)]
 
 
-    # Coordonnées de pour du 8 classes
-
-    # coords_val = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (8, 1), (9, 1), (10, 1), (13, 0), (4, 0)]
-    # coords_test = [(4, 3), (1, 3), (1, 4), (5, 4), (10, 0), (14, 0), (14, 1)]
-    # dark_coords = [(8, 4), (8, 5), (9, 4), (9, 5), (10, 4), (10, 5), (11, 3), (11, 4), (11, 5), (12, 3), (12, 4), (12, 5), (13,4), (13, 5), (14, 4), (14, 5), (15, 3), (15, 4), (15, 5)]
-    
-    # Coordonnées pour du 17 classes
+   
+    # Creating the train, validation and test datasets
     
     coords_val = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (9, 1), (10, 1), (13, 0), (4, 0)]
     coords_test = [(4, 3), (1, 3), (1, 4), (5, 4), (10, 0), (14, 0), (14, 1), (0, 0), (8, 1)]
@@ -67,6 +68,7 @@ def load(activate_pca = True, pca_explained_variance = 0.9999):
     test_labels = np.array([labels[coords_test[i][0], coords_test[i][1]] for i in range(len(coords_test))])
 
     # Calculating weights of classes and setting weight of class 0 to zero.
+    
     classes, counts = np.unique(labels, return_counts=True)
     counts[0] = 1
     weights = np.sum(counts)/counts
